75b203f1dec549230d9d8076d9e72c5dc8a4cd99
Minsu Kim <syt07100@gmail.com>
2023-08-18 10:56:44 +0900

    feat: BaseTimeEntity추가

---
+++ src/main/java/com/moing/backend/global/entity/BaseTimeEntity.java
@@ -0,0 +1,28 @@
+package com.moing.backend.global.entity;
+
+import jodd.typeconverter.impl.LocalDateTimeConverter;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.Column;
+import javax.persistence.Convert;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class BaseTimeEntity {
+
+    @CreatedDate
+    @Column(updatable = false)
+    private LocalDateTime createdDate;
+
+    @LastModifiedDate
+    private LocalDateTime lastModifiedDate;
+
+}
