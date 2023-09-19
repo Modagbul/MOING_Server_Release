d3c3b5a4930787442c81bdaf46e63d6ec42ea25f
정 승 연 <53565255+seungyeonnnnnni@users.noreply.github.com>
2023-09-19 15:59:49 +0900

    feat : 반복미션 조회 보드 구현

---
+++ src/main/java/com/moing/backend/domain/mission/application/dto/res/RepeatMissionBoardRes.java
@@ -0,0 +1,12 @@
+package com.moing.backend.domain.mission.application.dto.res;
+
+public class RepeatMissionBoardRes {
+    private Long missionId;
+    private String dueTo; //요일
+    private String title;
+    private String status;
+    private int num;
+    private int done;
+
+
+}
