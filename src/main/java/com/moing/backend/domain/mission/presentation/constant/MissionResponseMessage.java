5ab163a2c85001349e186906ddc160c6709f4880
정 승 연 <53565255+seungyeonnnnnni@users.noreply.github.com>
2023-12-04 11:25:04 +0900

    fix : 미션 종료 & API 문서 작성

--- src/main/java/com/moing/backend/domain/mission/presentation/constant/MissionResponseMessage.java
+++ src/main/java/com/moing/backend/domain/mission/presentation/constant/MissionResponseMessage.java
@@ -9,6 +9,7 @@ public enum MissionResponseMessage {
     CREATE_MISSION_SUCCESS("미션 생성을 완료 했습니다"),
     READ_MISSION_SUCCESS("미션 조회를 완료 했습니다"),
     UPDATE_MISSION_SUCCESS("미션 수정을 완료 했습니다"),
+    END_MISSION_SUCCESS("미션 종료를 완료 했습니다"),
     DELETE_MISSION_SUCCESS("미션 삭제를 완료 했습니다"),
     RECOMMEND_MISSION_SUCCESS("미션 추천을 위한 팀 카테고리를 조회 했습니다");
 
