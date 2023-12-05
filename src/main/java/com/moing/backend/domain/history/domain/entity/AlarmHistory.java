8bb4f01869644af5fcc217ca1bc2413f822f398e
minsu20 <syt07100@gmail.com>
2023-12-06 01:09:44 +0900

    feat: AlarmHistory 엔티티 추가

---
+++ src/main/java/com/moing/backend/domain/history/domain/entity/AlarmHistory.java
@@ -0,0 +1,45 @@
+package com.moing.backend.domain.history.domain.entity;
+
+import com.moing.backend.domain.member.domain.entity.Member;
+import com.moing.backend.global.entity.BaseTimeEntity;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Builder
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class AlarmHistory extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "alarm_history_id")
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private AlarmType type;
+
+    @Column(nullable = false)
+    private String path;
+
+    private String idInfo;
+
+    private Long receiverId;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String body;
+
+    @Column(nullable = false)
+    private String name;
+
+    private boolean isRead;
+}
