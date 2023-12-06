b42e367faca7a611a64a5df119941ab535d13779
정 승 연 <53565255+seungyeonnnnnni@users.noreply.github.com>
2023-12-06 23:04:51 +0900

    fix : 글자수 제한 에러 수정 과연

--- src/main/java/com/moing/backend/domain/missionArchive/application/dto/req/MissionArchiveReq.java
+++ src/main/java/com/moing/backend/domain/missionArchive/application/dto/req/MissionArchiveReq.java
@@ -2,12 +2,15 @@ package com.moing.backend.domain.missionArchive.application.dto.req;
 
 import lombok.*;
 
+import javax.validation.constraints.Size;
+
 @Builder
 @NoArgsConstructor
 @Getter
 public class MissionArchiveReq {
 
     private String status;
+    @Size(min = 1, max = 4000)
     private String archive; //사진일 경우 파일명, 이외에는 text,link
 
     @Builder
