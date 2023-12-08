75fc19675a1bbf33142d22c92fe956e1fbd09185
minsu20 <syt07100@gmail.com>
2023-12-08 22:18:36 +0900

    feat: 소모임장 권한 함수 추가

--- src/main/java/com/moing/backend/domain/team/application/service/DisbandTeamUseCase.java
+++ src/main/java/com/moing/backend/domain/team/application/service/DisbandTeamUseCase.java
@@ -5,7 +5,6 @@ import com.moing.backend.domain.member.domain.service.MemberGetService;
 import com.moing.backend.domain.team.application.dto.response.DeleteTeamResponse;
 import com.moing.backend.domain.team.domain.entity.Team;
 import com.moing.backend.domain.team.domain.service.TeamGetService;
-import com.moing.backend.domain.team.exception.NotAuthByTeamException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -23,12 +22,8 @@ public class DisbandTeamUseCase {
     public DeleteTeamResponse disbandTeam(String socialId, Long teamId) {
         Member member = memberGetService.getMemberBySocialId(socialId);
         Team team = teamGetService.getTeamByTeamId(teamId);
-
-        if (checkLeaderUseCase.isTeamLeader(member, team)) {
-            team.deleteTeam();
-        } else {
-            throw new NotAuthByTeamException();
-        }
+        checkLeaderUseCase.validateTeamLeader(member, team);
+        team.deleteTeam();
         return new DeleteTeamResponse(teamId);
     }
 }
