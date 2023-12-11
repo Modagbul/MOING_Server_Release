e5475b5096ba65eb0c8510766e4fbf650e297f97
minsu20 <syt07100@gmail.com>
2023-12-12 00:26:12 +0900

    fix: 1명인 경우 바로 삭제되게 수정

--- src/main/java/com/moing/backend/domain/team/application/service/DisbandTeamUseCase.java
+++ src/main/java/com/moing/backend/domain/team/application/service/DisbandTeamUseCase.java
@@ -5,6 +5,8 @@ import com.moing.backend.domain.member.domain.service.MemberGetService;
 import com.moing.backend.domain.team.application.dto.response.DeleteTeamResponse;
 import com.moing.backend.domain.team.domain.entity.Team;
 import com.moing.backend.domain.team.domain.service.TeamGetService;
+import com.moing.backend.domain.teamMember.domain.entity.TeamMember;
+import com.moing.backend.domain.teamMember.domain.service.TeamMemberGetService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -18,12 +20,17 @@ public class DisbandTeamUseCase {
     private final MemberGetService memberGetService;
     private final TeamGetService teamGetService;
     private final CheckLeaderUseCase checkLeaderUseCase;
+    private final TeamMemberGetService teamMemberGetService;
 
     public DeleteTeamResponse disbandTeam(String socialId, Long teamId) {
         Member member = memberGetService.getMemberBySocialId(socialId);
         Team team = teamGetService.getTeamByTeamId(teamId);
         checkLeaderUseCase.validateTeamLeader(member, team);
         team.deleteTeam();
+        if (team.getNumOfMember() == 1) { // 1명인 경우 3일 유예기간 없음
+            TeamMember teamMember = teamMemberGetService.getTeamMember(member, team);
+            teamMember.deleteMember(team);
+        }
         return new DeleteTeamResponse(teamId);
     }
 }
