172bbaef70a399baaa538f29daf5f15471d92169
정 승 연 <53565255+seungyeonnnnnni@users.noreply.github.com>
2023-12-18 01:02:55 +0900

    fix : 다시 인증하기 로직에 s3 삭제 로직 추가

--- src/main/java/com/moing/backend/domain/missionArchive/application/service/MissionArchiveDeleteUseCase.java
+++ src/main/java/com/moing/backend/domain/missionArchive/application/service/MissionArchiveDeleteUseCase.java
@@ -23,6 +23,7 @@ import com.moing.backend.domain.missionState.domain.service.MissionStateDeleteSe
 import com.moing.backend.domain.missionState.domain.service.MissionStateQueryService;
 import com.moing.backend.domain.missionState.domain.service.MissionStateSaveService;
 import com.moing.backend.domain.team.domain.entity.Team;
+import com.moing.backend.global.utils.UpdateUtils;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -43,12 +44,11 @@ public class MissionArchiveDeleteUseCase {
 
     private final MemberGetService memberGetService;
 
-    private final IssuePresignedUrlUseCase getPresignedUrlUseCase;
-
-    private final MissionStateSaveService missionStateSaveService;
-    private final MissionStateUseCase missionStateUseCase;
     private final MissionStateDeleteService missionStateDeleteService;
     private final MissionStateQueryService missionStateQueryService;
+
+    private final UpdateUtils updateUtils;
+
 
 
     public Long deleteArchive(String userSocialId, Long missionId,Long count) {
@@ -67,6 +67,11 @@ public class MissionArchiveDeleteUseCase {
         // 반복미션이면서 오늘 이전에 한 인증은 인증 취소할 수 없도록
         if (mission.getType().equals(MissionType.REPEAT) && createdDate.toLocalDate().isBefore(today.toLocalDate())) {
             throw new NoAccessMissionArchiveException();
+        }
+
+        if (mission.getWay().equals(MissionWay.PHOTO)) {
+            String archive = deleteArchive.getArchive();
+            updateUtils.deleteImgUrl(archive);
         }
 
         missionArchiveDeleteService.deleteMissionArchive(deleteArchive);
