4bf95aa16b59dd91be4887273caa6ed1013fbb15
minsu20 <syt07100@gmail.com>
2024-02-03 21:25:33 +0900

    MNG-06 fix: 댓글 알림 안오는 오류 수정

--- src/main/java/com/moing/backend/domain/boardComment/application/service/SendCommentAlarmUseCase.java
+++ src/main/java/com/moing/backend/domain/boardComment/application/service/SendCommentAlarmUseCase.java
@@ -2,9 +2,6 @@ package com.moing.backend.domain.boardComment.application.service;
 
 import com.moing.backend.domain.board.domain.entity.Board;
 import com.moing.backend.domain.boardComment.domain.entity.BoardComment;
-import com.moing.backend.domain.history.application.dto.response.MemberIdAndToken;
-import com.moing.backend.domain.history.application.dto.response.NewUploadInfo;
-import com.moing.backend.domain.history.application.mapper.AlarmHistoryMapper;
 import com.moing.backend.domain.history.domain.entity.AlarmType;
 import com.moing.backend.domain.history.domain.entity.PagePath;
 import com.moing.backend.domain.member.domain.entity.Member;
@@ -17,12 +14,10 @@ import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
-import java.util.List;
-import java.util.Optional;
 
-import static com.moing.backend.domain.history.domain.entity.PagePath.MISSION_PATH;
+import java.util.Objects;
+
 import static com.moing.backend.global.config.fcm.constant.NewCommentUploadMessage.NEW_COMMENT_UPLOAD_MESSAGE;
-import static com.moing.backend.global.config.fcm.constant.NewNoticeUploadMessage.NEW_NOTICE_UPLOAD_MESSAGE;
 
 @Service
 @RequiredArgsConstructor
@@ -35,13 +30,15 @@ public class SendCommentAlarmUseCase {
         Member member = response.getMember();
         Team team = response.getTeam();
         Board board = response.getBoard();
+        Member receiver = board.getTeamMember().getMember();
 
         String title = NEW_COMMENT_UPLOAD_MESSAGE.title(comment.getContent());
         String body = NEW_COMMENT_UPLOAD_MESSAGE.body(member.getNickName(),board.getTitle());
 
         // 알림 보내기
-        eventPublisher.publishEvent(new SingleFcmEvent(board.getTeamMember().getMember(), title, body, createIdInfo(team.getTeamId(), board.getBoardId()), team.getName(), AlarmType.NEW_UPLOAD, PagePath.NOTICE_PATH.getValue()));
-
+        if(checkBoardWriter(receiver, member)){
+            eventPublisher.publishEvent(new SingleFcmEvent(receiver, title, body, createIdInfo(team.getTeamId(), board.getBoardId()), team.getName(), AlarmType.NEW_UPLOAD, PagePath.NOTICE_PATH.getValue(), receiver.isNewUploadPush()));
+        }
     }
 
     private String createIdInfo(Long teamId, Long boardId) {
@@ -49,5 +46,9 @@ public class SendCommentAlarmUseCase {
         jo.put("teamId", teamId);
         jo.put("boardId", boardId);
         return jo.toJSONString();
+    }
+
+    private boolean checkBoardWriter(Member boardWriter, Member commentWriter){
+        return !Objects.equals(boardWriter.getMemberId(), commentWriter.getMemberId());
     }
 }
