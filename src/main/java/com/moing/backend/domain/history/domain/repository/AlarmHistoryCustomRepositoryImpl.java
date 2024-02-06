0484ee641912fec587b4c9ec77638a29d61837c5
minsu20 <syt07100@gmail.com>
2024-02-06 14:31:33 +0900

    MNG-14 fix: 알림 히스토리 전체 조회할 때 모두 읽음 처리

--- src/main/java/com/moing/backend/domain/history/domain/repository/AlarmHistoryCustomRepositoryImpl.java
+++ src/main/java/com/moing/backend/domain/history/domain/repository/AlarmHistoryCustomRepositoryImpl.java
@@ -3,23 +3,28 @@ package com.moing.backend.domain.history.domain.repository;
 import com.moing.backend.domain.history.application.dto.response.GetAlarmHistoryResponse;
 import com.moing.backend.domain.history.application.dto.response.QGetAlarmHistoryResponse;
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import com.querydsl.jpa.impl.JPAUpdateClause;
 
 import javax.persistence.EntityManager;
 import java.util.List;
 
 import static com.moing.backend.domain.history.domain.entity.QAlarmHistory.alarmHistory;
+import static com.moing.backend.domain.team.domain.entity.QTeam.team;
 
 public class AlarmHistoryCustomRepositoryImpl implements AlarmHistoryCustomRepository {
 
     private final JPAQueryFactory queryFactory;
 
+    private final EntityManager em;
+
     public AlarmHistoryCustomRepositoryImpl(EntityManager em) {
         this.queryFactory = new JPAQueryFactory(em);
+        this.em = em;
     }
 
     @Override
     public List<GetAlarmHistoryResponse> findAlarmHistoriesByMemberId(Long memberId) {
-        return queryFactory.select(new QGetAlarmHistoryResponse(alarmHistory.id,
+        List<GetAlarmHistoryResponse> response= queryFactory.select(new QGetAlarmHistoryResponse(alarmHistory.id,
                         alarmHistory.type,
                         alarmHistory.path,
                         alarmHistory.idInfo,
@@ -32,8 +37,21 @@ public class AlarmHistoryCustomRepositoryImpl implements AlarmHistoryCustomRepos
                 .where(alarmHistory.receiverId.eq(memberId))
                 .orderBy(alarmHistory.createdDate.desc())
                 .fetch();
+
+        readAlarmHistory(memberId);
+        return response;
     }
 
+    private void readAlarmHistory(Long memberId){
+        queryFactory
+                .update(alarmHistory)
+                .set(alarmHistory.isRead, true)
+                .where(alarmHistory.receiverId.eq(memberId))
+                        .execute();
+
+        em.flush();
+        em.clear();
+    }
     @Override
     public String findUnreadAlarmCount(Long memberId) {
         Long count = queryFactory.select(alarmHistory.count())
