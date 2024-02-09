1e54f2b3ac8af33cd429f62ade9082d1712c9779
minsu20 <syt07100@gmail.com>
2024-02-09 23:27:04 +0900

    MNG-17 feat: 댓글 작성자에게도 알림 가게 기능 추가

--- src/main/java/com/moing/backend/domain/boardComment/domain/repository/BoardCommentCustomRepository.java
+++ src/main/java/com/moing/backend/domain/boardComment/domain/repository/BoardCommentCustomRepository.java
@@ -1,8 +1,14 @@
 package com.moing.backend.domain.boardComment.domain.repository;
 
 import com.moing.backend.domain.boardComment.application.dto.response.GetBoardCommentResponse;
+import com.moing.backend.domain.history.application.dto.response.NewUploadInfo;
 import com.moing.backend.domain.teamMember.domain.entity.TeamMember;
+
+import java.util.List;
+import java.util.Optional;
 
 public interface BoardCommentCustomRepository {
     GetBoardCommentResponse findBoardCommentAll(Long boardId, TeamMember teamMember);
+
+    Optional<List<NewUploadInfo>> findNewUploadInfo(Long memberId, Long boardId);
 }
