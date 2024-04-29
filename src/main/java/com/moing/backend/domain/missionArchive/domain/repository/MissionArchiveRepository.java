4f26f849f95a8c28302408d89624f3169a231d3c
정 승 연 <53565255+seungyeonnnnnni@users.noreply.github.com>
2024-04-29 18:02:15 +0900

    fix : MissionState 패키지 삭제

--- src/main/java/com/moing/backend/domain/missionArchive/domain/repository/MissionArchiveRepository.java
+++ src/main/java/com/moing/backend/domain/missionArchive/domain/repository/MissionArchiveRepository.java
@@ -5,7 +5,6 @@ import com.moing.backend.domain.member.domain.entity.Member;
 import com.moing.backend.domain.mission.domain.entity.constant.MissionStatus;
 import com.moing.backend.domain.missionArchive.domain.entity.MissionArchive;
 import com.moing.backend.domain.missionArchive.domain.entity.MissionArchiveStatus;
-import com.moing.backend.domain.missionState.domain.repository.MissionArchiveStateCustomRepository;
 import com.querydsl.core.Tuple;
 import feign.Param;
 import org.hibernate.annotations.NamedNativeQuery;
@@ -22,7 +21,7 @@ import java.util.Map;
 import java.util.Optional;
 
 @Repository
-public interface MissionArchiveRepository extends JpaRepository<MissionArchive, Long>,MissionArchiveCustomRepository , MissionArchiveStateCustomRepository {
+public interface MissionArchiveRepository extends JpaRepository<MissionArchive, Long>,MissionArchiveCustomRepository {
 
     @Query("select m from MissionArchive as m where m.member =:memberId" )
     Optional<List<MissionArchive>> findByMemberId(@Param("memberId") Long memberId);
--- src/main/java/com/moing/backend/domain/missionArchive/exception/NoAccessMissionArchiveException.java
+++ src/main/java/com/moing/backend/domain/missionArchive/exception/NoAccessMissionArchiveException.java
@@ -1,10 +1,9 @@
 package com.moing.backend.domain.missionArchive.exception;
 
-import com.moing.backend.domain.missionState.exception.MissionStateException;
 import com.moing.backend.global.response.ErrorCode;
 import org.springframework.http.HttpStatus;
 
-public class NoAccessMissionArchiveException extends MissionStateException {
+public class NoAccessMissionArchiveException extends MissionArchiveException {
     public NoAccessMissionArchiveException() {
         super(ErrorCode.NO_MORE_ARCHIVE_ERROR, HttpStatus.NOT_FOUND);
 
