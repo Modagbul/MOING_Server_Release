4ffc33556e45b076af6444865a0dc59df3493739
minsu20 <syt07100@gmail.com>
2024-05-28 00:17:43 +0900

    fix: import 추가

--- src/main/java/com/moing/backend/domain/report/presentation/ReportController.java
+++ src/main/java/com/moing/backend/domain/report/presentation/ReportController.java
@@ -3,7 +3,7 @@ package com.moing.backend.domain.report.presentation;
 import com.moing.backend.domain.report.application.service.ReportCreateUseCase;
 import com.moing.backend.global.config.security.dto.User;
 import com.moing.backend.global.response.SuccessResponse;
-import lombok.RequiredArgsConstructor;
+import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import static com.moing.backend.domain.report.presentation.constant.ReportResponseMessage.CREATE_REPORT_SUCCESS;
 
 @RestController
-@RequiredArgsConstructor
+@AllArgsConstructor
 @RequestMapping("/api/report")
 public class ReportController {
 
