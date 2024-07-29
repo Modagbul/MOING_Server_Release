7fdcea4ea4f6a209589cfbaa3d8e82b3becfba64
정 승 연 <53565255+seungyeonnnnnni@users.noreply.github.com>
2024-07-29 18:33:28 +0900

    MNG-27 fix : 목표보드 불꽃 성장률 표시를 위한 점수 집계 방식 변경

--- src/main/java/com/moing/backend/domain/teamScore/application/service/TeamScoreGetUseCase.java
+++ src/main/java/com/moing/backend/domain/teamScore/application/service/TeamScoreGetUseCase.java
@@ -24,23 +24,21 @@ public class TeamScoreGetUseCase {
         Long level = teamScore.getLevel();
         Long score = teamScore.getScore();
 
-
         return TeamScoreRes.builder()
-                .score(score / getMaxScore(level) * 100)
+                .score((long) (score / getMaxScore(level) * 100))
                 .level(level)
                 .build();
 
     }
 
-    public Long getMaxScore(Long level) {
+    public Float getMaxScore(Long level) {
         final int[] steps = {1, 2, 26, 46, 71};
-        final long[] maxScores = {40, 60, 80, 100, 120};
+        final float[] maxScores = {40, 60, 80, 100, 120};
 
         int index = 0;
         int stepSize = steps.length - 1;
 
         for (int i = stepSize; i > 0; i--) {
-            System.out.println(steps[i-1]+" "+level+" "+steps[i]);
             if (steps[stepSize] <= level) {
                 index = stepSize;
             }
@@ -51,31 +49,7 @@ public class TeamScoreGetUseCase {
 
             }
         }
-        System.out.println(maxScores[index]);
         return maxScores[index];
 
     }
-//
-//    public void updateScore(Long score) {
-//
-//        int newStep = getStep(this.level, this.score + score);
-//
-//        this.score += score;
-//
-//        if (this.score < 0) { // 점수 차감 / score down + level down
-//            this.updateLevel(ScoreStatus.MINUS);
-//        }
-//        else { // 점수 획득. score up /  score down + level up
-//
-//            if ((40 + (newStep * 15L) <= this.score)) { // score down + level up
-//                this.updateLevel(ScoreStatus.PLUS);
-//
-//            } else { //  score up
-////                this.score += score;
-//            }
-//        }
-
-
-//    }
-
 }
